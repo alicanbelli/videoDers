9e982e62c770c76e37b2b6512d6b97eb46451f4e
alicanbelli <67042825+alicanbelli@users.noreply.github.com>
2020-06-27 10:31:12 +0300

    Update ders00.java
    github dan javaya commit gonderdim

--- src/VideoDers/ders00.java
+++ src/VideoDers/ders00.java
@@ -4,6 +4,8 @@ public class ders00 {
 
 	public static void main(String[] args) {
 		System.out.println("gitHub a da bu degisikligi yukledim");
+		System.out.println("bu satiri da github dan eclipse gonderdim");
+
 		
 		int a,b;
 		
